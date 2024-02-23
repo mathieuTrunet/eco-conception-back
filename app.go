b62144ddba57aecfc5a4d24b1e9d6fa76a66e877
mathieuTrunet <mathieu1508@gmail.com>
2024-02-23 19:55:59 +0100

    .

--- app.go
+++ app.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+type RequestBody struct {
+	Data []struct {
+		Text string `json:"text" binding:"required"`
+	} `json:"data" binding:"required"`
+}
+
 func main() {
 
-	r := gin.Default()
+	application := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "pong"}) })
+	application.POST("/api/page/download", func(response *gin.Context) {
+		var requestBody RequestBody
 
-	r.Run()
+		if error := response.ShouldBindJSON(&requestBody); error != nil {
+			response.JSON(http.StatusBadRequest, gin.H{"error": error.Error()})
+			return
+		}
+
+		template, error := template.ParseFiles("template.html")
+		if error != nil {
+			response.JSON(http.StatusInternalServerError, gin.H{"error": "template not found"})
+			return
+		}
+
+		htmlBuffer := new(strings.Builder)
+
+		error = template.Execute(htmlBuffer, requestBody)
+		if error != nil {
+			response.JSON(http.StatusInternalServerError, gin.H{"error": "parsing error"})
+			return
+		}
+
+		response.Header("Content-Type", "text/html")
+		response.Header("Content-Disposition", "attachment; filename=builded_page.html")
+
+		response.String(http.StatusOK, htmlBuffer.String())
+	})
+	application.Run(":8080")
 
 }
